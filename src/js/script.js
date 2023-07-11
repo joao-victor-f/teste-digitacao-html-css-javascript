8e9b82568dfcb4fcfeb12f6b23f3bd3cc47b865b
victor <joaovicif2021@gmail.com>
2023-07-10 21:23:57 -0300

    Feita lógica de popular array com palavras aleatorias

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const palavrasContainer = document.querySelector('.palavras-container');
+
+const geraIndiceAleatorio = (arrayLength) => {
+    return Math.floor(Math.random() * (arrayLength + 1));
+}
+
+const popularContainer = () => {
+    for (let i = 0; i < palavras.length; i++) {
+        let span = `<span>${palavras[geraIndiceAleatorio(palavras.length)]}</span>`;
+        palavrasContainer.innerHTML += span;
+    }
+}
+
+window.onload = popularContainer;
